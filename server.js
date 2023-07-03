a46f06b51ee02433f1e73ebde825b0d8be29b1b6
Mr.Hunter <sameersheikhmansoor@gmail.com>
2023-07-03 10:08:25 +0530

    Aaj bnega game

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve the static files
+app.use(express.static(__dirname + '/public'));
+
+// Handle incoming WebSocket connections
+io.on('connection', socket => {
+  // Handle offer message
+  socket.on('offer', offer => {
+    // Broadcast offer to all other connected clients except the sender
+    socket.broadcast.emit('incoming-call', offer);
+  });
+
+  // Handle answer message
+  socket.on('answer', answer => {
+    // Broadcast answer to all other connected clients except the sender
+    socket.broadcast.emit('answer', answer);
+  });
+
+  // Handle ICE candidate message
+  socket.on('ice-candidate', candidate => {
+    // Broadcast ICE candidate to all other connected clients except the sender
+    socket.broadcast.emit('ice-candidate', candidate);
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(__dirname + '/public');
+  console.log(`Server running on port ${port}`);
+});
